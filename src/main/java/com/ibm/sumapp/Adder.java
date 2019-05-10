34e06040d157c54e42ed676302921cbb236a6bd9
jconallen <jconallen@us.ibm.com>
2019-05-10 10:36:24 -0400

    this fixes #51

--- src/main/java/com/ibm/sumapp/Adder.java
+++ src/main/java/com/ibm/sumapp/Adder.java
@@ -7,9 +7,9 @@ public class Adder {
 		int sum = a + b;
 		
 		// introduce an intermittent error
- 		if( a == 3 ) { 
- 			sum = a*a + b*b;
- 		} 
+// 		if( a == 3 ) { 
+// 			sum = a*a + b*b;
+// 		} 
 		
 		return sum;
 	}
